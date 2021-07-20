4edc373ff28acaeb641d79fd4e60a2b3e1878b6f
mseagalsmichael <ms.moncada22@ciaf.edu.co>
2021-07-19 19:01:48 -0500

    Se crea vista del issues central

--- src/app/modules/monitoring/components/issues-central/issues-central.component.ts
+++ src/app/modules/monitoring/components/issues-central/issues-central.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-issues-central',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IssuesCentralComponent implements OnInit {
 
-  constructor() { }
+  dataSource : MatTableDataSource<any>;
+  displayedColumns : string[] = ['happenedDate','restoredDate','reason','city','observation','supervisor'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor() { 
+    this.dataSource = new MatTableDataSource();
+
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
---
+++ src/app/modules/monitoring/interfaces/issue-central-interface.ts
@@ -0,0 +1,8 @@
+export interface IssueCentralInterface {
+    happenedDate:Date,
+    restoredDate:Date,
+    reason:string,
+    city:string,
+    observation:string,
+    supervisor:string
+}
